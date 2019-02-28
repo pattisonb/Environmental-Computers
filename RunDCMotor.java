58b31f77f96eee00f9546a330654afe8767450b7
pattisonb <47764431+pattisonb@users.noreply.github.com>
2019-02-28 16:33:34 -0600

    Add files via upload

---
+++ RunDCMotor.java
@@ -0,0 +1,22 @@
+import rxtxrobot.*;
+
+public class RunDCMotor {
+    static RXTXRobot r = new ArduinoNano();
+    
+    public static void main(String[] args) {
+        
+		r.setPort("/dev/tty.usbmodem14301"); // Set port to COM3
+		r.connect();
+                
+                //move 3m
+		r.runTwoPCAMotor(0, 200, 1, 200, 5000);
+    }
+    
+    public static void RotateRobotPositive() {
+        r.runTwoPCAMotor(0, 200, 1, -200, 5000);
+    }
+    
+    public static void RotateRobotNegative() {
+        r.runTwoPCAMotor(0, -200, 1, 200, 5000);
+    }
+}
