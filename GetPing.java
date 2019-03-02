5bead81b7ca941ba42aa70aa030f92c1ab059399
jameshhe <hhe990219@hotmail.com>
2019-03-02 15:53:59 -0600

    Add files via upload

---
+++ GetPing.java
@@ -0,0 +1,31 @@
+import rxtxrobot.*;
+
+public class GetPing
+{
+
+	public static void main(String[] args)
+	{
+		//we have 3 ping sensors
+		//do we need to write codes for them seperately?
+		int LEFT_PIN = 12;
+		int RIGHT_PIN = 13;
+		int CENTER_PIN = 14;
+		RXTXRobot r = new ArduinoNano(); // Create RXTXRobot object
+		r.setPort("COM3"); // Set the port to COM3
+		r.connect();
+		for (int x=0; x < 100; ++x)
+		{
+			//Read the ping sensor value, which is connected to pin 12
+			int leftDistance = r.getPing(LEFT_PIN);
+			int rightDistance = r.getPing(RIGHT_PIN);
+			int centerDistance = r.getPing(CENTER_PIN);
+			System.out.println("Response: " + leftDistance + " cm");
+			//think about different scenarios 
+			if(leftDistance < 10) { //if distance is smaller than 10cm (or smaller/larger?)
+				//stop the robot and turn it 
+			}
+			r.sleep(300);//what is this for?
+		}
+		r.close();
+	}
+}
