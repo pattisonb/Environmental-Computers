6dc2df2400b4848b27b943c732ee797c90f94a25
jameshhe <hhe990219@hotmail.com>
2019-03-02 16:33:00 -0600

    Update GetPing.java

--- GetPing.java
+++ GetPing.java
@@ -19,10 +19,15 @@ public class GetPing
 			int leftDistance = r.getPing(LEFT_PIN);
 			int rightDistance = r.getPing(RIGHT_PIN);
 			int centerDistance = r.getPing(CENTER_PIN);
-			System.out.println("Response: " + leftDistance + " cm");
 			//think about different scenarios 
-			if(leftDistance < 10) { //if distance is smaller than 10cm (or smaller/larger?)
-				//stop the robot and turn it 
+			if(leftDistance < 10 && centerDistance < 10) { //if there are walls on the left side and front of the robot
+				r.runTwoPCAMotor(0, 200, 1, -200, 5000);//turn right; need to change the time 5000  
+			}
+			if(rightDistance < 10 && centerDistance < 10) { //if there are walls on the right side and front of the robot
+				r.runTwoPCAMotor(0, 200, 1, -200, 5000);//turn left 
+			}
+			if(centerDistance < 10){//if there is a wall in front of the robot
+				r.runTwoPCAMotor(0, 200, 1, -200, 10000); //turn left until the robot turns 180 degrees around (or not? or we can make it turn left or right?) 
 			}
 			r.sleep(300);//what is this for?
 		}
