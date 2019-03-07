60d3e7a7d52a421b7e64bfbdeb425c9896afe3ed
pattisonb <47764431+pattisonb@users.noreply.github.com>
2019-03-06 18:16:32 -0600

    Update CinderblockTest.java

--- CinderblockTest.java
+++ CinderblockTest.java
@@ -3,7 +3,7 @@ import rxtxrobot.*;
 public class CinderblockTest {
     public static void main(String[] args)
 	{
-            int PING_PIN = 5;
+            int PING_PIN = 12;
             int distance = 0;
             int motorLeft = 14;
             int motorRight = 15;
@@ -12,17 +12,16 @@ public class CinderblockTest {
             r.setPort("/dev/tty.usbmodem14301"); // Set the port to correct port
             r.connect();
             
-            for (int i = 0; i < 10; i++) {  //don't know how long to iterate yet because we don't know how long the course will be
+            for (int i = 0; i < 20; i++) {  //don't know how long to iterate yet because we don't know how long the course will be
                 //currently set to run for ten feet if runDCmotor call is accurate to 3 inches
                 distance = r.getPing(PING_PIN);
-                System.out.println(distance);
                 if (distance <= 25) { //ping after moving or before?
                         System.out.print("X");
                     }
                 else if (distance >= 25){
                         System.out.print("O");
                     }
-                r.sleep(300);
+                r.sleep(500);
                 r.runTwoPCAMotor(motorRight, -100, motorLeft, 175, 50); //calibrate to move robot about 3 inches after pinging
                 r.sleep(500);
             }
