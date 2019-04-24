2497fa4598049a2e823741b198a81d4a1538f7e6
jameshhe <hhe990219@hotmail.com>
2019-04-24 10:58:12 -0500

    Update Quadrant2Auto.java

--- Quadrant2Auto.java
+++ Quadrant2Auto.java
@@ -8,28 +8,30 @@ public class Quadrant2Auto {
 		r.setPort("/dev/tty.usbmodem14301"); 
 		r.connect();
 
-                r.runTwoPCAMotor(14, 235, 15, -180, 3200);//move forward for x amount of time
-                RotateRobotPositive(1325);
-                r.sleep(1500);
-//                
-                FindBeacon('S');
-                r.sleep(2000);
-                r.runTwoPCAMotor(14, 235, 15, -170, 800);
-                RotateRobotPositive(100);
-            
-                r.sleep(200);
-                r.runTwoPCAMotor(14, 235, 15, -170, 880);
+                //move straight and then turn 90 degrees
+		r.runTwoPCAMotor(14, 235, 15, -180, 2600);//move forward for x amount of time
+		r.sleep(5000);
+		RotateRobotPositive(870);       
+				
+		//move straight
+                r.sleep(5000);
+                r.runTwoPCAMotor(14, 235, 15, -170, 2400);
+                
+                //arrive at the rescue zone
+                r.sleep(5000);
+                RotateRobotPositive(780);//turn right
+                r.sleep(5000);
+                r.runTwoPCAMotor(14, 205, 15, -140, 1100);//collect salamanders (do it at a lower speed or the salamanders would be pushed out!)
                 r.sleep(2500);
-                r.runPCATimedServo(9, 0, 3000);
+                r.runPCATimedServo(9, 180, 3000);//move the arm down to secure salamanders
                 r.sleep(1000);
 
                 r.runTwoPCAMotor(14, -235, 15, 235, 1800);
                 r.sleep(200);
-                FindBeacon('G');
                 RotateRobotPositive(100);
                 r.runTwoPCAMotor(14, 245, 15, -170, 3100);
                 
-                System.exit(0);            
+                System.exit(0);               
     }
     
     public static void RotateRobotPositive(int time) {
