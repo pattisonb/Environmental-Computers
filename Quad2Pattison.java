43e6e4a6cf5f2ee0b3a12fb95a3f2807001b6f42
jameshhe <hhe990219@hotmail.com>
2019-04-25 17:30:49 -0500

    Update Quad2Pattison.java

--- Quad2Pattison.java
+++ Quad2Pattison.java
@@ -1,3 +1,5 @@
+//5:30 pm, 4/25/19
+//ran at 13.2V
 import rxtxrobot.*;
 
 public class Quadrant2Auto {
@@ -5,35 +7,40 @@ public class Quadrant2Auto {
     
     public static void main(String[] args) {
         
-		r.setPort("/dev/tty.usbmodem14301"); 
+		r.setPort("COM3"); 
 		r.connect();
-                   r.runPCAServo(9, 45);
+                 r.runPCAServo(9, 45);//lift up the arm
                    r.sleep(300);
-                r.runTwoPCAMotor(14, 200, 15, -170, 2900);//move forward for x amount of time
-                //RotateRobotPositive(100);
+                r.runTwoPCAMotor(14, 200, 15, -150, 3100);//go straight
                 r.sleep(1500);
-                RotateRobotPositive(150);
+                RotateRobotPositive(150);//turn right
                 r.sleep(1500);
-                FindBeacon('S');
+                r.runTwoPCAMotor(14, 220, 15, -160, 100);//go forward a little bit
                 r.sleep(2000);
-                RotateRobotPositive(50);
+                FindBeacon('S');//try to find s
+                r.sleep(2000);
+                RotateRobotPositive(55);//turn right
                 r.sleep(750);
-                r.runTwoPCAMotor(14, 220, 15, -200, 1400);            
+                r.runTwoPCAMotor(14, 230, 15, -190, 1400);//go to the rescuing zone            
                 r.sleep(200);
                 r.runTwoPCAMotor(14, 225, 15, -200, 700);
                 r.sleep(1500);
-                r.runPCAServo(9, 180);                
+                r.runPCAServo(9, 180);//close the arm (secure the salamanders)                
                 r.sleep(1000);
 
-                r.runTwoPCAMotor(14, -235, 15, 235, 1900);
+                r.runTwoPCAMotor(14, -235, 15, 235, 2000);//go backwards
                 r.sleep(2000);
-                FindBeacon('G');
+                RotateRobotPositive(200);//rotate and go forward so that it's easier to find g
+                r.sleep(2000);
+                r.runTwoPCAMotor(14, 235, 15, -235, 200);
+                r.sleep(2000);
+                FindBeacon('G');//find g
                 //RotateRobotPositive(415);
                 r.runTwoPCAMotor(14, 245, 15, -180, 900);
                 r.sleep(1500);
-                r.runTwoPCAMotor(14, 245, 15, -190, 1100);
+                r.runTwoPCAMotor(14, 245, 15, -190, 500);
                 r.runPCAServo(9, 45);
-                
+               
                 r.close();            
     }
     
@@ -47,17 +54,22 @@ public class Quadrant2Auto {
 	    //motor strength 150, channels 0 and 1, time: .8 seconds
 	}
     public static void FindBeacon (char beaconChar) {
+    			int i = 0;
                 char beaconInput = '0';
                 char beaconOutput = beaconChar; //character sent out by beacon CHANGE THIS!!!
                 for(;;) {
                             beaconInput = r.getIRChar();
                             System.out.println(beaconInput);
-			if(beaconInput == beaconOutput) {
-                            break;
+                          	if(beaconInput == beaconOutput) {
+                          		/*if(i <= 4) {
+                          			 RotateRobotPositive(20);//turn right a little bit
+                          		}*/
+                          		break;
 			}
                 r.sleep(200);
                 r.runTwoPCAMotor(14, 140, 15, 140, 130);
                 r.sleep(1000);
+                i++;
                 }
                 }
 }
