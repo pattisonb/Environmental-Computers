b7b37a582093e0ccf945cf583a47d0acd61fc264
elisego45 <47865195+elisego45@users.noreply.github.com>
2019-04-25 23:24:56 -0500

    Update Quadrant4Auto-updated.java

--- Quadrant4Auto-updated.java
+++ Quadrant4Auto-updated.java
@@ -1,55 +1,64 @@
 import rxtxrobot.*;
 
 public class Quadrant4Auto {
-    static RXTXRobot r = new ArduinoNano();
+   static RXTXRobot r = new ArduinoNano();
     
     public static void main(String[] args) {
                 r = new ArduinoUno();
 		r.setPort("COM3"); 
 		r.connect();
-                //r.runPCAServo(8, 90);
-               // r.sleep(200);				this was the former code to go down ramp in q4
-				//r.runTwoPCAMotor(14, 235, 15, -200, 1000);
-		
-                r.runTwoPCAMotor(14, 235, 15, -200, 400);
-                r.sleep(200);
-                RotateRobotPositive(300);
-                r.sleep(400);
-                r.runTwoPCAMotor(14, 235, 15, -200, 400);
-                r.sleep(3000);
-                FindBeaconNeg('K');
-                r.sleep(200);
-                r.runTwoPCAMotor(14, 235, 15, -200, 1000);
-                r.sleep(200);
-                r.runPCAServo(8, 90);
-                r.sleep(3000);
-r.runTwoPCAMotor(14, 235, 15, -200, 600);
-                double celsius = ((getThermistorReading() - 677.7186471))/-6.93063423; 
-                System.out.printf("In Degrees Celsius: %.2f\n", celsius);
-                r.runPCAServo(8, 180);
-                r.sleep(200);
-                r.runTwoPCAMotor(14, -235, 15, 200, 200);
-                r.sleep(200);
-                RotateRobotNegative(870);
-                r.sleep(200);
-                r.runTwoPCAMotor(14, 235, 15, -200, 1000);
-                r.sleep(200);
-                RotateRobotPositive(1300);
-                r.sleep(200);
-                r.runTwoPCAMotor(14, 235, 15, -200, 600);
-                r.sleep(200);
-                FindBeaconPos('N');
-                RotateRobotPositive(100);
-               
-				r.runTwoPCAMotor(14, 235, 15, -200, 2000);
-                
-				RotateRobotPositive(845);
-				r.sleep(2000);
-                r.runTwoPCAMotor(14, 495, 15, -435, 1350);//battery is 12.9
+				r.runTwoPCAMotor(14, 200, 15, -200, 6000);
                 r.sleep(8000);
-                r.runTwoPCAMotor(14, 265, 15, -200, 1800);
+                r.runPCAServo(9, 45);
+                r.runPCAServo(8, 0);
+            	RotateRobotNegative(90);
+		r.runTwoPCAMotor(14, -215, 15, 235, 700); //drive backwards after ramp
+		RotateRobotNegative(90);
+                r.sleep(500);
+
+                FindBeaconPos('K');
+                r.sleep(200);
+                r.runTwoPCAMotor(14, 235, 15, -200, 400); //drive to temp site (400 at 13.2V)
+                r.sleep(3000);
+                RotateRobotPositive(145); //adjust for temp site
+                r.sleep(500);
+                r.runTwoPCAMotor(14, 235, 15, -200, 600); //finsih driving to temp site
+                r.sleep(1500);
+                r.runPCAServo(8, 180);
+                r.sleep(3000);
+                double celsius = (getThermistorReading() - 677.7186471)/-6.93063423; 
+                System.out.printf("In Degrees Celsius: %.2f\n", celsius);
+                r.runPCAServo(8, 0);
                 
-//                
+                r.sleep(1000);
+                r.runTwoPCAMotor(14, -215, 15, 260, 95); //back up a little
+                r.sleep(1000);
+                RotateRobotNegative(220); //roatte less than 90 so it can start to find 'N' at a closer distance; aka drive diagonally across the quadrant
+                r.sleep(1000);
+                r.runTwoPCAMotor(14, 235, 15, -180, 3350); //drive forwards to wall (2850 at 13.7V, 3050 at 13.2V, 3350 at 13.1V)
+                r.sleep(2000);
+                RotateRobotNegative(80);
+                r.sleep(2000);
+                
+                FindBeaconPos('N'); //find n
+                r.runTwoPCAMotor(14, 235, 15, -180, 1700); //drive to n (1400 at 13.5V, 1800 at 13.3V
+                r.sleep(5000);
+              
+                r.runTwoPCAMotor(14, -215, 15, 260, 55); //back up a little but
+		RotateRobotPositive(885); //rotatte to face bridge
+		r.sleep(2000);
+                r.runTwoPCAMotor(14, -215, 15, 260, 30); //set up to drive over bridg (backwards)
+                r.sleep(500); 
+                r.runTwoPCAMotor(14, 235, 15, -200, 100); //setting up for drive over bridge (forwards)
+                  r.runTwoPCAMotor(14, 235, 15, -200, 25);
+                r.sleep(500);
+                r.runTwoPCAMotor(14, 400, 15, -425, 25); //get over ledge
+                r.sleep(500);
+                r.runTwoPCAMotor(14, 450, 15, -520, 1000);
+                r.sleep(1000);                
+                r.runTwoPCAMotor(14, 235, 15, -200, 900);
+                r.sleep(500);
+                
                 System.exit(0);
     }
     
@@ -76,29 +85,29 @@ r.runTwoPCAMotor(14, 235, 15, -200, 600);
         public static void FindBeaconNeg (char beaconChar) {
                 char beaconInput = '0';
                 char beaconOutput = beaconChar; //character sent out by beacon CHANGE THIS!!!
-                for(int i = 0; i <= 180; i+=10) {
+                for(;;) {
                             beaconInput = r.getIRChar();
                             System.out.println(beaconInput);
 			if(beaconInput == beaconOutput) {
                             break;
 			}
                 r.sleep(200);
-                RotateRobotNegative(100);
+                RotateRobotNegative(50);
                 r.sleep(1000);
                 }
                 }
-                public static void FindBeaconPos (char beaconChar) {
+           public static void FindBeaconPos (char beaconChar) {
                     r.refreshDigitalPins();
                 char beaconInput = '0';
                 char beaconOutput = beaconChar; //character sent out by beacon CHANGE THIS!!!
-                for(int i = 0; i <= 180; i+=10) {
+                for(;;) {
                             beaconInput = r.getIRChar();
                             System.out.println(beaconInput);
 			if(beaconInput == beaconOutput) {
                             break;
 			}
                 r.sleep(200);
-                RotateRobotPositive(100);
+                RotateRobotPositive(30);
                 r.sleep(1000);
                 }
                 }
